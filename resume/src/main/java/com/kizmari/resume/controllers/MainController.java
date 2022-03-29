fe8bec5725816cc6b3ba450b4a376b598c41bfa1
Kizmari <kizmari@yandex.ru>
2022-03-29 13:53:19 +0300

    Not HTML

---
+++ resume/src/main/java/com/kizmari/resume/controllers/MainController.java
@@ -0,0 +1,18 @@
+package com.kizmari.resume.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class MainController {
+	
+	@GetMapping ("/")
+	public String home (Model model) {
+		model.addAttribute ("title", "Главная страница");
+		return "home";
+	}
+	
+	
+
+}
