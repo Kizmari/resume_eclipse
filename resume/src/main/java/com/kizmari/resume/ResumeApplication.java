2290934c0c75f9e1b20b12f707d9ec1ca5b20a0d
Kizmari <kizmari@yandex.ru>
2022-03-29 17:07:05 +0300

    first page home.html

--- resume/src/main/java/com/kizmari/resume/ResumeApplication.java
+++ resume/src/main/java/com/kizmari/resume/ResumeApplication.java
@@ -3,6 +3,8 @@ package com.kizmari.resume;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class ResumeApplication {
 
